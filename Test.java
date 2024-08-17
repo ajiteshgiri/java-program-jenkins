6cbc380233239c18ff1fb569be9337f528390d2f
ajiteshgiri <ajiteshkumar4512@gmail.com>
2024-08-17 07:21:39 +0530

    change new date print

--- Test.java
+++ Test.java
@@ -1,7 +1,12 @@
+import java.util.*;
+
+
 class Test{
 	public static void main(String[] args) {
 		System.out.println("Hello ajitesh");
 		System.out.println("jenkins from java program");
+		System.out.println(new Date());
+
 
 	}
 }
